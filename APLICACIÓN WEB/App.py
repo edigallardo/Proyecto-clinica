64364fc9075e8462f2e56158033ebac55601cffa
sornoza137 <esornoza3606@utm.edu.ec>
2024-11-27 15:52:28 -0500

    rutas para tratamiento

--- APLICACIÓN WEB/App.py
+++ APLICACIÓN WEB/App.py
@@ -41,3 +41,49 @@ def eliminar_doctor(id):
     cursor.execute("DELETE FROM Doctores WHERE id = %s", (id,))
     db.commit()
     return redirect(url_for('mostrar_doctores'))
+
+# Rutas para Tratamientos
+@app.route('/tratamientos')
+def mostrar_tratamientos():
+    cursor = db.cursor()
+    cursor.execute("""
+        SELECT Tratamientos.id, Citas.id, Medicamentos.nombre, Tratamientos.dosis, Tratamientos.duracion
+        FROM Tratamientos
+        JOIN Citas ON Tratamientos.cita_id = Citas.id
+        JOIN Medicamentos ON Tratamientos.medicamento_id = Medicamentos.id
+    """)
+    tratamientos = cursor.fetchall()
+    return render_template('tratamientos.html', tratamientos=tratamientos)
+
+@app.route('/agregar_tratamiento', methods=['POST'])
+def agregar_tratamiento():
+    cita_id = request.form['cita_id']
+    medicamento_id = request.form['medicamento_id']
+    dosis = request.form['dosis']
+    duracion = request.form['duracion']
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO Tratamientos (cita_id, medicamento_id, dosis, duracion) VALUES (%s, %s, %s, %s)", (cita_id, medicamento_id, dosis, duracion))
+    db.commit()
+    return redirect(url_for('mostrar_tratamientos'))
+
+@app.route('/edit_tratamiento/<int:id>', methods=['GET', 'POST'])
+def editar_tratamiento(id):
+    cursor = db.cursor()
+    if request.method == 'POST':
+        cita_id = request.form['cita_id']
+        medicamento_id = request.form['medicamento_id']
+        dosis = request.form['dosis']
+        duracion = request.form['duracion']
+        cursor.execute("UPDATE Tratamientos SET cita_id = %s, medicamento_id = %s, dosis = %s, duracion = %s WHERE id = %s", (cita_id, medicamento_id, dosis, duracion, id))
+        db.commit()
+        return redirect(url_for('mostrar_tratamientos'))
+    cursor.execute("SELECT * FROM Tratamientos WHERE id = %s", (id,))
+    tratamiento = cursor.fetchone()
+    return render_template('edit_tratamiento.html', tratamiento=tratamiento)
+
+@app.route('/delete_tratamiento/<int:id>', methods=['POST'])
+def eliminar_tratamiento(id):
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM Tratamientos WHERE id = %s", (id,))
+    db.commit()
+    return redirect(url_for('mostrar_tratamientos'))
